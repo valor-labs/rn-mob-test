0ea189eebccaebd2fda73713aa7b8bed515bc765
“thegrep01” <den.belotserkovets@gmail.com>
2019-10-17 18:41:26 +0300

    Feature: dropdown

--- src/views/forms/for-me-form.tsx
+++ src/views/forms/for-me-form.tsx
@@ -1,7 +1,53 @@
-import { View } from 'react-native';
 import React from 'react';
+import { List, Item, Input, Label } from 'native-base';
+import { Dropdown } from 'react-native-material-dropdown';
+import { ScrollView } from 'react-native';
 
 export function FromMeForm() {
-    
-    return <View></View>;
+    const data = [
+        {
+            value: 'Male',
+        },
+        {
+            value: 'Female',
+        },
+    ];
+    const data2 = [
+        {
+            value: 'Locally recruited',
+        },
+    ];
+
+    return (
+        <ScrollView style={{ flex: 1, paddingLeft: 16, paddingRight: 16 }}>
+            <List>
+                <Item floatingLabel style={{ marginBottom: 21 }}>
+                    <Label>Name</Label>
+                    <Input />
+                </Item>
+                <Item floatingLabel style={{ marginBottom: 21 }}>
+                    <Label>Surname</Label>
+                    <Input />
+                </Item>
+                <Item floatingLabel style={{ marginBottom: 21 }}>
+                    <Label>Email</Label>
+                    <Input />
+                </Item>
+                <Item floatingLabel style={{ marginBottom: 21 }}>
+                    <Label>Phone number</Label>
+                    <Input />
+                </Item>
+                <Item floatingLabel style={{ marginBottom: 21 }}>
+                    <Label>Agency</Label>
+                    <Input />
+                </Item>
+                <Item floatingLabel style={{ marginBottom: 21 }}>
+                    <Label>Reason for Visit</Label>
+                    <Input />
+                </Item>
+                <Dropdown label='Gender' data={data} style={{ marginBottom: 21 }} />
+                <Dropdown label='Locally recruited' data={data2} style={{ marginBottom: 21 }} />
+            </List>
+        </ScrollView>
+    );
 }
--- src/views/forms/forms.tsx
+++ src/views/forms/forms.tsx
@@ -1,15 +1,21 @@
 // import { SafeAreaView } from 'react-native';
 import React from 'react';
 import { FromMeForm } from './for-me-form';
-import { Container, Header, Tabs, Tab, Title, Body, Footer, Button, Text } from 'native-base';
+import { Container, Header, Tabs, Tab, Title, Body, Footer, Button, Text, Left, Icon, Right } from 'native-base';
 
-export function Forms() {
+export function Forms({ navigation }: any) {
     return (
         <Container>
-            <Header hasTabs style={{ backgroundColor: '#D8D9DB' }}>
+            <Header hasTabs noShadow style={{ backgroundColor: '#D8D9DB' }}>
+                <Left>
+                    <Button transparent onPress={() => navigation.pop()}>
+                        <Icon style={{ color: 'black' }} ios='close' android='close' name='close' type='AntDesign' />
+                    </Button>
+                </Left>
                 <Body>
                     <Title style={{ color: 'black' }}>Header</Title>
                 </Body>
+                <Right />
             </Header>
             <Tabs tabBarUnderlineStyle={{ backgroundColor: 'black' }}>
                 <Tab
@@ -17,6 +23,7 @@ export function Forms() {
                     tabStyle={{ backgroundColor: '#D8D9DB' }}
                     activeTabStyle={{ backgroundColor: '#D8D9DB' }}
                     activeTextStyle={{ color: 'black' }}
+                    textStyle={{ color: 'black' }}
                 >
                     <FromMeForm />
                 </Tab>
@@ -25,19 +32,27 @@ export function Forms() {
                     tabStyle={{ backgroundColor: '#D8D9DB' }}
                     activeTabStyle={{ backgroundColor: '#D8D9DB' }}
                     activeTextStyle={{ color: 'black' }}
+                    textStyle={{ color: 'black' }}
                 >
                     <FromMeForm />
                 </Tab>
             </Tabs>
             <Footer
                 style={{
-                    shadowOpacity: 0.14,
-                    shadowRadius: 4,
-                    shadowColor: 'black',
-                    shadowOffset: { height: 0, width: 0 },
+                    borderColor: 'transparent', // Required to show shadows on Android for some reason !?!?
+                    shadowColor: '#000',
+                    shadowOffset: {
+                        width: 0,
+                        height: 0,
+                    },
+                    shadowOpacity: 0.3,
+                    shadowRadius: 5,
+                    backgroundColor: 'white',
+                    elevation: 15,
+                    minHeight: 70,
                 }}
             >
-                <Button block dark style={{ width: 328, height: 40, marginTop: 15, borderRadius: 4 }}>
+                <Button block dark style={{ width: 328, height: 40, marginTop: 20, borderRadius: 4 }}>
                     <Text>Send request</Text>
                 </Button>
             </Footer>
