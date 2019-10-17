00c49ca213116f71dfd33019b4805347e0291c7c
“thegrep01” <den.belotserkovets@gmail.com>
2019-10-17 13:30:04 +0300

    Update: styles refactor

--- src/views/login/Login.tsx
+++ src/views/login/Login.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import { View } from 'react-native';
 import { Container, Text, H1, H3, Input, Item, Button } from 'native-base';
+import { loginStyle } from './Login.style';
 
 function LoginScreen() {
     return (
-        <Container style={{ flex: 1, flexDirection: 'column', alignItems: 'center', backgroundColor: '#ECF1F4' }}>
-            <View style={{ paddingTop: 65.5, paddingBottom: 32.5 }}>
+        <Container style={loginStyle.wrapper}>
+            <View style={loginStyle.logo}>
                 <H1>Logo</H1>
             </View>
             <View>
                 <H3>Login</H3>
             </View>
-            <View style={{ width: 297, paddingTop: 24 }}>
-                <Item regular style={{ borderColor: 'black', marginBottom: 18, borderRadius: 4 }}>
+            <View style={loginStyle.loginWrapper}>
+                <Item regular style={loginStyle.inputContainer}>
                     <Input
                         placeholder='Email'
-                        style={{ backgroundColor: 'white', height: 40, borderRadius: 4 }}
+                        style={loginStyle.inputStyle}
                         autoCompleteType='email'
                         keyboardType='email-address'
                     />
                 </Item>
-                <Button block dark style={{ borderRadius: 4 }}>
+                <Button block dark style={loginStyle.button}>
                     <Text>Continue</Text>
                 </Button>
             </View>
