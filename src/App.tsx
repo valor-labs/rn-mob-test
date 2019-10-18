5e3d4e55a79b0cda9841371bebf1b8af20780c42
“thegrep01” <den.belotserkovets@gmail.com>
2019-10-18 16:42:00 +0300

    Update: refactor

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,8 @@ import LoginScreen from './views/login/login';
 import { ListScreen } from './views/list/list';
 import { AuthRoutes } from './common/constants/routes';
 import { Forms } from './views/forms/forms';
-import { CalendarModal } from './views/modal/calendar/calendar';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
-import { fadeIn } from 'react-navigation-transitions';
-
-const FormStack = createStackNavigator(
-    {
-        Main: Forms,
-        Modal: { screen: CalendarModal },
-    },
-    {
-        mode: 'modal',
-        headerMode: 'none',
-        transparentCard: true,
-        transitionConfig: () => fadeIn(),
-    }
-);
 
 const RootStack = createStackNavigator(
     {
